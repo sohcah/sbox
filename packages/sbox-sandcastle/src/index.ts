a0bebfdbf43fd2df50d967190b02b30c4796c3c6
Sam Hindess <mail@sohcah.dev>
2026-07-28 21:07:02 +0100

    Bump to version 0.2.4

--- packages/sbox-sandcastle/src/index.ts
+++ packages/sbox-sandcastle/src/index.ts
@@ -12,7 +12,7 @@ export {
 } from "@sohcah/sbox";
 
 export const PACKAGE_NAME = "@sohcah/sbox-sandcastle" as const;
-export const PACKAGE_VERSION = "0.2.3" as const;
+export const PACKAGE_VERSION = "0.2.4" as const;
 
 export { createSboxSandcastleProvider, type SboxSandcastleOptions } from "./provider.js";
 
